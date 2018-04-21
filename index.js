231334a2678f236c0f055717b31c01d3775d1a3b
ana.rajner@gmail.com <ana.rajner@gmail.com>
2018-04-21 10:54:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function iterativeLog(array1, callback){
+  array1.forEach(callback);
+}
+
+function iterate(index, element, array) {
+  
+}
+
+function doToArray (array1, callback){
+  array1.forEach(callback);
+}
